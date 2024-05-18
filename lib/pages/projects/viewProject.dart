7600b502462e72d63097657cfc48a70b579f56d7
sharifu majid <sharifumajid3@gmail.com>
2024-05-18 12:33:54 +0300

    bottom navigation

--- lib/pages/projects/viewProject.dart
+++ lib/pages/projects/viewProject.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:tanzify_app/components/spinners/spinkit.dart';
 import 'package:tanzify_app/data/providers/projectProvider.dart';
 import 'package:tanzify_app/models/project/projectModal.dart';
+import 'package:tanzify_app/pages/constants.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class ViewProject extends StatefulWidget {
   final ProjectModel project;
@@ -21,6 +24,7 @@ class _ViewProjectState extends State<ViewProject> {
     final isLoading = projectProvider.isLoading;
     return Scaffold(
       appBar: AppBar(
+        elevation: 2,
         leading: IconButton(
           onPressed: () {
             Navigator.pop(context);
@@ -51,30 +55,53 @@ class _ViewProjectState extends State<ViewProject> {
             )
           : Container(
               height: fullHeight,
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    child: Row(
+                      children: [
+                        const Text(
+                          "Posted ",
+                          style: TextStyle(fontSize: 11),
+                        ),
+                        Text(
+                          widget.project.created != null
+                              ? timeago.format(
+                                  DateTime.parse(widget.project.created!))
+                              : "Date not available",
+                          style:
+                              const TextStyle(color: Colors.grey, fontSize: 11),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  SizedBox(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          padding: const EdgeInsets.only(right: 5),
+                          alignment: Alignment.topLeft,
+                          child: const Icon(
+                            Icons.campaign_rounded,
+                            color: Constants.primaryColor,
+                          ),
+                        ),
+                        Expanded(
+                          flex: 9,
+                          child: Text(widget.project.description ?? ""),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-    );
-  }
-
-  Widget _backButton() {
-    return InkWell(
-      onTap: () {
-        Navigator.pop(context);
-      },
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Row(
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.only(left: 0, top: 10, bottom: 10),
-              child: const Icon(Icons.keyboard_arrow_left, color: Colors.black),
-            ),
-            const Text('Back',
-                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500))
-          ],
-        ),
-      ),
     );
   }
 }
