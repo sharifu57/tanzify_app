aadc98ce242b224001476172d891ec01c3ac5c37
sharifu majid <sharifumajid3@gmail.com>
2024-05-20 07:55:00 +0300

    commit on new images

--- lib/pages/constants.dart
+++ lib/pages/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Constants {
-  static const primaryColor = Color.fromARGB(255, 0, 50, 91);
+  static const primaryColor = Color(0xFF00325B);
   static const accentColor = Color(0xFF0065B7);
   static const fillColor = Color(0xfff3f3f4);
   static const borderColor = Color.fromARGB(255, 230, 230, 230);
